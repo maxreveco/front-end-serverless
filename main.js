b8a46a65f6f664868f500e7a00a66b38515412c4
Max Reveco <max.revecog@hotmail.com>
2022-03-25 15:24:35 -0300

    se agregan funcionalidades JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let mealsState = []
 const stringToHtml = (string) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(string, 'text/html')
@@ -6,30 +7,82 @@ const stringToHtml = (string) => {
 
 const renderItem = (item) => {
     const element = stringToHtml(`<li data-id="${item._id}">${item.name}</li>`)
+
+    element.addEventListener('click', () => {
+        const mealsList = document.getElementById('meals-list')
+        Array.from(mealsList.children).forEach(x => x.classList.remove('selected'))
+        element.classList.add('selected')
+        const mealsIdInput = document.getElementById('meals-id')
+        mealsIdInput.value = item._id
+        console.log(mealsIdInput.value);
+    })
+
+    return element
+}
+
+const renderOrder = (order, meals) => {
+    const meal = meals.find(meal => meal._id === order.meal_id)
+    console.log(meal)
+    const element = stringToHtml(`<li data-id="${order._id}">${meal.name} - ${order.user_id}</li>`)
+
     return element
 }
 
 window.onload = () => {
-    fetch('https://serverless-maxreveco.vercel.app/api/meals/'//,
-        // {
-        //     method: 'GET',
-        //     mode: 'cors',
-        //     cache: 'no-cache',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     redirect: 'follow',
-        //     body: JSON.stringify({ user: 'lala', password: 'chanchito' })
-        // }
-    )
+    const orderForm = document.getElementById('order')
+    orderForm.onsubmit = (e) => {
+        const submit = document.getElementById('submit')
+        submit.setAttribute('disabled', true)
+        e.preventDefault()
+        const mealId = document.getElementById('meals-id')
+        const mealIdValue = mealId.value
+        if (!mealIdValue) {
+            alert('Debe seleccionar un plato')
+            return
+        }
+        const order = {
+            meal_id: mealIdValue,
+            user_id: 'chanchito feliz!'
+        }
+
+        fetch('https://serverless-maxreveco.vercel.app/api/orders/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(x => x.json())
+            .then(respuesta => {
+                const renderedOrder = renderOrder(respuesta, mealsState)
+                const ordersList = document.getElementById('orders-list')
+                ordersList.appendChild(renderedOrder)
+                submit.removeAttribute('disabled')
+            })
+
+
+    }
+
+    fetch('https://serverless-maxreveco.vercel.app/api/meals/')
         .then(response => response.json())
         .then(data => {
+            mealsState = data
             const mealsList = document.getElementById('meals-list')
             const submit = document.getElementById('submit')
             const listItems = data.map(renderItem)
             mealsList.removeChild(mealsList.firstElementChild)
             listItems.forEach(element => mealsList.appendChild(element))
             submit.removeAttribute('disabled')
+            fetch('https://serverless-maxreveco.vercel.app/api/orders/')
+                .then(response => response.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('orders-list')
+                    const listOrders = ordersData.map(orderData => renderOrder(orderData, data))
+
+                    ordersList.removeChild(ordersList.firstElementChild)
+                    listOrders.forEach(element => ordersList.appendChild(element))
+                })
         })
+
+
 }
